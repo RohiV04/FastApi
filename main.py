badf1c495cfdd8ac8caabd2296161364193eca0e
Rohith <95613210+RohiV04@users.noreply.github.com>
2023-07-01 21:30:40 +0530

    first commit

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from router.note import note
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-PORT=9000
+
 app.include_router(note)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=PORT)
+    uvicorn.run(app, host="0.0.0.0", port=9000)
